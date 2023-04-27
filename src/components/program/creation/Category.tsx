579d21fe04a930f7d7b3f9369e6eecddb2597519
Yujaebin <dbwpqls9998@naver.com>
2023-04-27 23:50:48 +0900

    feat: 카테고리 저장

--- src/components/program/creation/Category.tsx
+++ src/components/program/creation/Category.tsx
@@ -10,7 +10,7 @@ import {
   StudyOptions,
   stateOptions,
 } from "../../../docs/Docs";
-
+import { useForm, Controller } from "react-hook-form";
 interface ButtonProps {
   increaseStep: () => void;
 }
@@ -25,58 +25,91 @@ const Category = (props: ButtonProps) => {
     setFirstCategory(change);
     console.log(firstCategory);
   };
+  const {
+    control,
+    register,
+    getValues,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+    props.increaseStep();
+  };
+  const onError = (error: any) => {
+    console.log(error);
+  };
   return (
     <BasicForm>
-      <p style={{ marginTop: "5rem", marginLeft: "3rem", fontSize: "1rem" }}>
-        카테고리 선택하기
-      </p>
-      <div style={{ marginTop: "1rem", marginLeft: "3rem" }}>
-        <Select
-          isMulti
-          name="colors"
-          options={stateOptions}
-          className="basic-multi-select"
-          classNamePrefix="select"
-          onChange={(change) => firstCategoryChange(change)}
-        />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <p style={{ marginTop: "5rem", marginLeft: "3rem", fontSize: "1rem" }}>
+          카테고리 선택하기
+        </p>
+        <div style={{ marginTop: "1rem", marginLeft: "3rem" }}>
+          <Select
+            isMulti
+            name="colors"
+            options={stateOptions}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={(change) => firstCategoryChange(change)}
+          />
+          <Controller
+            control={control}
+            name="colors"
+            render={({ field: { onChange, ...field } }) => (
+              <Select
+                {...field}
+                options={stateOptions}
+                isMulti
+                onChange={(change, data) => {
+                  firstCategoryChange(change);
+                  onChange(change);
+                }}
+              />
+            )}
+          />
+        </div>
 
-      <p style={{ marginTop: "3rem", marginLeft: "3rem", fontSize: "1rem" }}>
-        선택한 대표 카테고리들의 관심사를 선택해주세요
-      </p>
-      {firstCategory.map((value: CategoryValue, index: number) => {
-        return (
-          <div style={{ marginTop: "1rem", marginLeft: "3rem" }} key={index}>
-            <p
-              style={{
-                fontSize: "0.8rem",
-                marginBottom: "0.5rem",
-                marginTop: "2rem",
-              }}
-            >
-              <FaBook style={{ marginRight: "0.3rem", color: "#399DA3" }} />
-              {value.label}
-            </p>
-            <Select
-              isMulti
-              name="colors"
-              options={value.value}
-              className="basic-multi-select"
-              classNamePrefix="select"
-            />
-          </div>
-        );
-      })}
+        <p style={{ marginTop: "3rem", marginLeft: "3rem", fontSize: "1rem" }}>
+          선택한 대표 카테고리들의 관심사를 선택해주세요
+        </p>
+        {firstCategory.map((value: CategoryValue, index: number) => {
+          return (
+            <div style={{ marginTop: "1rem", marginLeft: "3rem" }} key={index}>
+              <p
+                style={{
+                  fontSize: "0.8rem",
+                  marginBottom: "0.5rem",
+                  marginTop: "2rem",
+                }}
+              >
+                <FaBook style={{ marginRight: "0.3rem", color: "#399DA3" }} />
+                {value.label}
+              </p>
+              <Select
+                isMulti
+                name="colors"
+                options={value.value}
+                className="basic-multi-select"
+                classNamePrefix="select"
+              />
+            </div>
+          );
+        })}
 
-      <Button
-        variant="contained"
-        color="secondary"
-        style={{ fontSize: "1rem", left: "45%", marginTop: "8.3rem" }}
-        sx={{ height: "2.2rem", width: "11rem" }}
-        onClick={props.increaseStep}
-      >
-        상세정보 입력하기
-      </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          style={{ fontSize: "1rem", left: "45%", marginTop: "8.3rem" }}
+          sx={{ height: "2.2rem", width: "11rem" }}
+          // onClick={props.increaseStep}
+          type="submit"
+          disabled={isSubmitting}
+        >
+          상세정보 입력하기
+        </Button>
+      </form>
     </BasicForm>
   );
 };
