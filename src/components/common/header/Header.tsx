75129c424acf65cae7bba87e54090605bb5b5f15
Yujaebin <dbwpqls9998@naver.com>
2023-05-15 23:35:25 +0900

    feat: 헤더부분 알림 css 추가

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,17 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import Grid from "@mui/material/Unstable_Grid2";
 import "./Header.css";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { useLocation, useNavigate } from "react-router-dom";
 import { logOut } from "../../../slice/user/loginSlice";
+import ClearIcon from "@mui/icons-material/Clear";
+import Modal from "react-modal";
 
 const Header = () => {
+  const customStyles = {
+    overlay: {
+      background: "rgba(125, 125, 125, 0.2)",
+      backdropFilter: "blur(4px)",
+    },
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "25px",
+      boxShadow: "0 3px 10px 0 rgba(0, 0, 0, 0.15)",
+      width: "23.813rem",
+      height: "47.563rem",
+      backgroundColor: "#f5f5f5",
+      padding: "0",
+    },
+  };
   const dispatch = useAppDispatch();
   const userNo = useAppSelector((state) => state.login.object.USER_NO);
   const userGb = useAppSelector((state) => state.login.object.user_gb);
+  const [modalIsOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
+  const modalOn = () => {
+    setIsOpen(true);
+  };
+  const modalOff = () => {
+    setIsOpen(false);
+  };
   const NavStyle = styled.div`
     width: 35%;
     display: flex;
@@ -44,19 +73,47 @@ const Header = () => {
                 <div>S E A R C H</div>
                 <div>C L A S S</div>
               </NavStyle>
-              {userNo === null ? (
-                <ImageStyle
-                  onClick={() => {
-                    navigate("/");
-                  }}
+              <AlertWithLogin>
+                <CircleStyle onClick={modalOn}>Alert</CircleStyle>
+                <Modal
+                  isOpen={modalIsOpen}
+                  style={customStyles}
+                  ariaHideApp={false}
                 >
-                  Login
-                </ImageStyle>
-              ) : (
-                <ImageStyle onClick={() => dispatch(logOut())}>
-                  {userNo}
-                </ImageStyle>
-              )}
+                  <ModalodalHeader>
+                    <div
+                      style={{
+                        marginLeft: "1.5rem",
+                        fontSize: "1.2rem",
+                        fontFamily: "NotoSansRegular",
+                      }}
+                    >
+                      알림
+                    </div>
+
+                    <ClearIcon
+                      onClick={modalOff}
+                      sx={{ marginRight: "1.5rem", cursor: "pointer" }}
+                    />
+                  </ModalodalHeader>
+                  <ModalDate>
+                    <p>2023.03.15 목요일</p>
+                  </ModalDate>
+                </Modal>
+                {userNo === null ? (
+                  <CircleStyle
+                    onClick={() => {
+                      navigate("/");
+                    }}
+                  >
+                    Login
+                  </CircleStyle>
+                ) : (
+                  <CircleStyle onClick={() => dispatch(logOut())}>
+                    {userNo}
+                  </CircleStyle>
+                )}
+              </AlertWithLogin>
             </HeaderStyle>
           </Grid>
           <Grid xs={1}></Grid>
@@ -79,7 +136,7 @@ const LogoStyle = styled.div`
   background-color: green;
 `;
 
-const ImageStyle = styled.div`
+const CircleStyle = styled.div`
   cursor: pointer;
   height: 2.5rem;
   width: 2.5rem;
@@ -90,6 +147,25 @@ const ImageStyle = styled.div`
   font-size: 0.8rem;
   text-align: center;
   font-weight: bold;
+  margin-right: 1rem;
 `;
-
+const AlertWithLogin = styled.div`
+  display: flex;
+`;
+const ModalodalHeader = styled.div`
+  display: flex;
+  height: 3rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+const ModalDate = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ececec;
+  height: 2rem;
+  font-family: Inter;
+  font-size: 0.5rem;
+  color: #676767;
+`;
 export default Header;
