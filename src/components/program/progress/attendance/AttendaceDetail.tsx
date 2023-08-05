464fc3d2c81e54c9753c47303b5ab5b888736064
Yujaebin <dbwpqls9998@naver.com>
2023-08-05 11:12:11 +0900

    출석

--- src/components/program/progress/attendance/AttendaceDetail.tsx
+++ src/components/program/progress/attendance/AttendaceDetail.tsx
@@ -3,14 +3,47 @@ import styled from "@emotion/styled";
 import { FaRegCheckCircle } from "react-icons/fa";
 import axios from "axios";
 import { Button } from "@mui/material";
-import Icon from "./Icon";
 import { useAppSelector } from "../../../../store/hooks";
-
-const Attend = {
-  mento_no: null,
-};
-
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 const AttendenceDetail = () => {
+  const peopleInfo = [
+    {
+      name: "홍길동",
+      school: "숭의여자고등학교",
+      age: "25세",
+      출석: 5,
+      지각: 1,
+      결석: 0,
+    },
+    {
+      name: "김길동",
+      school: "서강남초고등학교",
+      age: "23세",
+      출석: 4,
+      지각: 1,
+      결석: 1,
+    },
+    {
+      name: "박길동",
+      school: "서울여자고등학교",
+      age: "22세",
+      출석: 4,
+      지각: 1,
+      결석: 1,
+    },
+    {
+      name: "정길동",
+      school: "숭의남자고등학교",
+      age: "21세",
+      출석: 3,
+      지각: 2,
+      결석: 1,
+    },
+  ];
   const user_gb = useAppSelector((state) => state.login.object.user_gb);
   return (
     <AttendForm>
@@ -26,35 +59,26 @@ const AttendenceDetail = () => {
       <Attendbox>
         {user_gb === "MENTO" && (
           <p>
-            <Studentbox>
-              <Studentlist>
-                <p>icon</p>
-                <p>박서영</p>
-                <p>숭의여자고등학교</p>
-                <p>25세</p>
-              </Studentlist>
-              <StudentAttend>
-                <p>현재 총 수업 수: 4회</p>
-                <p>출석 5회</p>
-                <p>지각 1회</p>
-                <p>결석 1회</p>
-              </StudentAttend>
-            </Studentbox>
-
-            <Studentbox>
-              <Studentlist>
-                <p>icon</p>
-                <p>박서영</p>
-                <p>숭의여자고등학교</p>
-                <p>25세</p>
-              </Studentlist>
-              <StudentAttend>
-                <p>현재 총 수업 수: 4회</p>
-                <p>출석 5회</p>
-                <p>지각 1회</p>
-                <p>결석 1회</p>
-              </StudentAttend>
-            </Studentbox>
+            {peopleInfo.map((value, index) => (
+              <>
+                <Studentbox>
+                  <Studentlist>
+                    <p>icon</p>
+                    <p>{value.name}</p>
+                    <p>{value.school}</p>
+                    <p>{value.age}</p>
+                  </Studentlist>
+                  <StudentAttend>
+                    <p>
+                      현재 총 수업 수: {value.결석 + value.지각 + value.출석}회
+                    </p>
+                    <p>출석 {value.출석}회</p>
+                    <p>지각 {value.지각}회</p>
+                    <p>결석 {value.결석}회</p>
+                  </StudentAttend>
+                </Studentbox>
+              </>
+            ))}
           </p>
         )}
         {user_gb === "MENTEE" && (
@@ -77,33 +101,50 @@ const AttendenceDetail = () => {
         )}
         <Checklist>
           <p style={{ fontSize: "0.8rem" }}>날짜 :</p>
-          <p>2022.02.31</p>
-          <p>진행차시 : 1차시 / 9차시</p>
-          <p>프로그램기간 : 2022.02.01 ~ 2022.09.21</p>
+          <p>2023.08.13</p>
+          <p>진행차시 : 6차시 / 9차시</p>
+          <p>프로그램기간 : 2023.08.13 ~ 2023.09.21</p>
         </Checklist>
         {user_gb === "MENTO" && (
-          <Studentbox>
-            <Studentlist>
-              <p>icon</p>
-              <p>박서영</p>
-              <p>숭의여자고등학교</p>
-              <p>25세</p>
-            </Studentlist>
-            <StudentAttendCheck>
-              <Attendanceicon>
-                <p style={{ lineHeight: "1.5rem" }}>출석</p>
-                <Icon></Icon>
-              </Attendanceicon>
-              <Perceiveicon>
-                <p style={{ lineHeight: "1.5rem" }}>지각</p>
-                <Icon></Icon>
-              </Perceiveicon>
-              <Absenticon>
-                <p style={{ lineHeight: "1.5rem" }}>결석</p>
-                <Icon></Icon>
-              </Absenticon>
-            </StudentAttendCheck>
-          </Studentbox>
+          <>
+            {peopleInfo.map((value, index) => (
+              <>
+                <Studentbox>
+                  <Studentlist>
+                    <p>icon</p>
+                    <p>{value.name}</p>
+                    <p>{value.school}</p>
+                    <p>{value.age}</p>
+                  </Studentlist>
+                  <StudentAttendCheck>
+                    <FormControl>
+                      <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                      >
+                        <FormControlLabel
+                          value="출석"
+                          control={<Radio color="secondary" />}
+                          label="출석"
+                        />
+                        <FormControlLabel
+                          value="지각"
+                          control={<Radio color="secondary" />}
+                          label="지각"
+                        />
+                        <FormControlLabel
+                          value="결석"
+                          control={<Radio color="secondary" />}
+                          label="결석"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </StudentAttendCheck>
+                </Studentbox>
+              </>
+            ))}
+          </>
         )}
         {user_gb === "MENTEE" && (
           <Studentbox>
@@ -112,23 +153,6 @@ const AttendenceDetail = () => {
               <p>23.02.13</p>
               <p>16:00 ~ 18:00</p>
             </Studentlist>
-            <StudentAttendCheck>
-              <Attendanceicon>
-                <p style={{ lineHeight: "1.5rem", marginLeft: "9rem" }}>출석</p>
-                <Icon></Icon>
-                <p
-                  style={{
-                    marginLeft: "1rem",
-                    fontSize: "0.9rem",
-                    color: "#FF8E41",
-                    lineHeight: "1.5rem",
-                    cursor: "pointer",
-                  }}
-                >
-                  문의하기
-                </p>
-              </Attendanceicon>
-            </StudentAttendCheck>
           </Studentbox>
         )}
       </Attendbox>
@@ -192,6 +216,7 @@ const StudentAttendCheck = styled.div`
   justify-content: space-between;
   margin: 1.5% 2% 1.8% 14%;
   font-size: 1rem;
+  align-items: center;
 `;
 const Checklist = styled.div`
   display: flex;
